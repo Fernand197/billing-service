ac7355e7338fb529aa6ebd9c1ddcd45504809d71
Fernand197 <tatchumf@gmail.com>
2022-12-02 16:14:14 +0100

    Init billing service

---
+++ src/main/java/com/openlab/openlabbillingservice/OpenlabBillingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.openlab.openlabbillingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenlabBillingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenlabBillingServiceApplication.class, args);
+    }
+
+}
